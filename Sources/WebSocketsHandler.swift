49e57734804145599f499456fb2aa546e709dfba
Kyle Jessup <badthing@gmail.com>
2016-06-10 13:09:02 -0400

    Updated for enum renaming

--- Sources/WebSocketsHandler.swift
+++ Sources/WebSocketsHandler.swift
@@ -25,7 +25,7 @@ func addWebSocketsHandler() {
     Routing.Routes["*"] = { request, response in StaticFileHandler().handleRequest(request: request, response: response) }
     
     // Add the endpoint for the WebSocket example system
-    Routing.Routes[.Get, "/echo"] = {
+    Routing.Routes[.get, "/echo"] = {
         request, response in
         
         // To add a WebSocket service, set the handler to WebSocketHandler.
