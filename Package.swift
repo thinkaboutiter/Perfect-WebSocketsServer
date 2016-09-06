a11e3a83e2250888204860be37c64c1362e444b5
Kyle Jessup <badthing@gmail.com>
2016-09-06 16:49:31 -0400

    Pinned dep versions

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 //
 //  Package.swift
-//  PerfectExamples
+//  PerfectExample-WebSocketsServer
 //
 //  Created by Kyle Jessup on 3/22/16.
 //	Copyright (C) 2016 PerfectlySoft, Inc.
@@ -25,7 +25,7 @@ let package = Package(
 		
 	],
 	dependencies: [
-		.Package(url:"https://github.com/PerfectlySoft/Perfect-HTTPServer.git", versions: Version(0,0,0)..<Version(10,0,0)),
-		.Package(url:"https://github.com/PerfectlySoft/Perfect-WebSockets.git", versions: Version(0,0,0)..<Version(10,0,0))
+		.Package(url:"https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
+		.Package(url:"https://github.com/PerfectlySoft/Perfect-WebSockets.git", majorVersion: 2, minor: 0)
 	]
 )
