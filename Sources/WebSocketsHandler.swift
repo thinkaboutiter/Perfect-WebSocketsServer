64ebfc4c2ddf47431c8499502bdc645fd311b5f8
Boyan Yankov <boyankov@yahoo.com>
2018-08-16 16:22:16 +0300

    updated `WebSocketsHandler.swift` - fixed identation

--- Sources/WebSocketsHandler.swift
+++ Sources/WebSocketsHandler.swift
@@ -22,15 +22,15 @@ import PerfectWebSockets
 import PerfectHTTP
 
 func makeRoutes() -> Routes {
-	
-	var routes = Routes()
+    
+    var routes = Routes()
     // Add a default route which lets us serve the static index.html file
-	routes.add(method: .get, uri: "*", handler: { request, response in
-		StaticFileHandler(documentRoot: request.documentRoot, allowResponseFilters: true).handleRequest(request: request, response: response)
-	})
+    routes.add(method: .get, uri: "*", handler: { request, response in
+        StaticFileHandler(documentRoot: request.documentRoot, allowResponseFilters: true).handleRequest(request: request, response: response)
+    })
     
     // Add the endpoint for the WebSocket example system
-	routes.add(method: .get, uri: "/echo", handler: {
+    routes.add(method: .get, uri: "/echo", handler: {
         request, response in
         
         // To add a WebSocket service, set the handler to WebSocketHandler.
@@ -47,8 +47,8 @@ func makeRoutes() -> Routes {
             return EchoHandler()
         }).handleRequest(request: request, response: response)
     })
-	
-	return routes
+    
+    return routes
 }
 
 // A WebSocket service handler must impliment the `WebSocketSessionHandler` protocol.
@@ -64,47 +64,45 @@ func makeRoutes() -> Routes {
 // A read timeout can be set with `WebSocket.readTimeoutSeconds`.
 // When the session is over call `WebSocket.close()`.
 class EchoHandler: WebSocketSessionHandler {
-	
-	// The name of the super-protocol we implement.
-	// This is optional, but it should match whatever the client-side WebSocket is initialized with.
-	let socketProtocol: String? = "echo"
-	
-	// This function is called by the WebSocketHandler once the connection has been established.
-	func handleSession(request: HTTPRequest, socket: WebSocket) {
-		
-		// Read a message from the client as a String.
-		// Alternatively we could call `WebSocket.readBytesMessage` to get binary data from the client.
-		socket.readStringMessage {
-			// This callback is provided:
-			//	the received data
-			//	the message's op-code
-			//	a boolean indicating if the message is complete (as opposed to fragmented)
-			string, op, fin in
-			
-			// The data parameter might be nil here if either a timeout or a network error, such as the client disconnecting, occurred.
-			// By default there is no timeout.
-			guard let string = string else {
-				// This block will be executed if, for example, the browser window is closed.
-				socket.close()
-				return
-			}
-			
-			// Print some information to the console for informational purposes.
-			print("Read msg: \(string) op: \(op) fin: \(fin)")
-			
-			// Echo the data we received back to the client.
-			// Pass true for final. This will usually be the case, but WebSockets has the concept of fragmented messages.
-			// For example, if one were streaming a large file such as a video, one would pass false for final.
-			// This indicates to the receiver that there is more data to come in subsequent messages but that all the data is part of the same logical message.
-			// In such a scenario one would pass true for final only on the last bit of the video.
-			socket.sendStringMessage(string: string, final: true) {
-				
-				// This callback is called once the message has been sent.
-				// Recurse to read and echo new message.
-				self.handleSession(request: request, socket: socket)
-			}
-		}
-	}
+    
+    // The name of the super-protocol we implement.
+    // This is optional, but it should match whatever the client-side WebSocket is initialized with.
+    let socketProtocol: String? = "echo"
+    
+    // This function is called by the WebSocketHandler once the connection has been established.
+    func handleSession(request: HTTPRequest, socket: WebSocket) {
+        
+        // Read a message from the client as a String.
+        // Alternatively we could call `WebSocket.readBytesMessage` to get binary data from the client.
+        socket.readStringMessage {
+            // This callback is provided:
+            //	the received data
+            //	the message's op-code
+            //	a boolean indicating if the message is complete (as opposed to fragmented)
+            string, op, fin in
+            
+            // The data parameter might be nil here if either a timeout or a network error, such as the client disconnecting, occurred.
+            // By default there is no timeout.
+            guard let string = string else {
+                // This block will be executed if, for example, the browser window is closed.
+                socket.close()
+                return
+            }
+            
+            // Print some information to the console for informational purposes.
+            print("Read msg: \(string) op: \(op) fin: \(fin)")
+            
+            // Echo the data we received back to the client.
+            // Pass true for final. This will usually be the case, but WebSockets has the concept of fragmented messages.
+            // For example, if one were streaming a large file such as a video, one would pass false for final.
+            // This indicates to the receiver that there is more data to come in subsequent messages but that all the data is part of the same logical message.
+            // In such a scenario one would pass true for final only on the last bit of the video.
+            socket.sendStringMessage(string: string, final: true) {
+                
+                // This callback is called once the message has been sent.
+                // Recurse to read and echo new message.
+                self.handleSession(request: request, socket: socket)
+            }
+        }
+    }
 }
-
-
